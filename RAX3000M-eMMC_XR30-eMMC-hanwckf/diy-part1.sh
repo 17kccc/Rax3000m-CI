64f27e7a10cf55dc9299248405d383fb01863a1a
17kccc <357365498@qq.com>
2025-03-22 00:23:55 +0800

    Update diy-part1.sh

--- RAX3000M-eMMC_XR30-eMMC-hanwckf/diy-part1.sh
+++ RAX3000M-eMMC_XR30-eMMC-hanwckf/diy-part1.sh
@@ -18,4 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #git clone https://github.com/messense/aliyundrive-webdav package/messense
 #git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
-git clone https://github.com/ntlf9t/luci-app-easymesh package/luci-app-easymesh
+#git clone https://github.com/ntlf9t/luci-app-easymesh package/luci-app-easymesh
